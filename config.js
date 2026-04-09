f41c38a361f785e60ef139c2aa1f51972f59820a
L1rtus <riqzzainsotka09@gmail.com>
2026-04-09 23:59:06 +0300

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-// config.js - НЕ ПУБЛИКУЙ ЭТОТ ФАЙЛ! Добавь в .gitignore
-export const SUPABASE_URL = 'https://hufcamhbzyksmhebreys.supabase.co';
-export const SUPABASE_KEY = 'sb_publishable_IovkOkcJ9osHjSiXD8wR-A_TX6aRxgI';
-export const ADMIN_PASSWORD = 'killuaebet';
